e67c034a902503b51618907db4439526d497b350
Mao-xy <15850563830@139.com>
2015-05-06 20:14:24 +0800

    add

---
+++ src/bussinesslogic/playertechbl/PlayerTechFind.java
@@ -0,0 +1,22 @@
+package bussinesslogic.playertechbl;
+
+import java.util.ArrayList;
+
+import test.data.PlayerHighInfo;
+import test.data.PlayerNormalInfo;
+
+public class PlayerTechFind {
+	//位置筛选考虑多位置和没位置的
+	//返回所有球员的基本数据
+	public ArrayList<PlayerNormalInfo> normalAll(String aveOrAll, int number,
+			String filter, String sort){
+		//拆分field.value
+		//拆分field.sortOrder
+		return null;
+	}
+	//返回所有球员的高阶数据,默认ave，不过滤
+	public ArrayList<PlayerHighInfo> highAll(int number, String sort){
+		return null;
+	}
+
+}
